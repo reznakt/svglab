d4a934cba8a6b3916a676dc1c8d91123e4047f88
Tomáš Režňák <tomas.reznak@volny.cz>
2024-11-28 13:22:44 +0100

    Allow configuring parser type

--- svglab/parse.py
+++ svglab/parse.py
@@ -1,28 +1,45 @@
-from typing import cast
+from collections import deque
+from typing import Literal
 
 import bs4
+from bs4 import BeautifulSoup
 
 from svglab.elements import Svg
 
 from .utils import SupportsRead
 
-
-class BeautifulSoup(bs4.BeautifulSoup):
-    """A wrapper around `bs4.BeautifulSoup` that uses the `lxml-xml` parser."""
-
-    def __init__(
-        self, markup: str | bytes | SupportsRead[str] | SupportsRead[bytes] = ""
-    ) -> None:
-        super().__init__(markup=markup, features="lxml-xml")
+type Parser = Literal["html.parser", "lxml", "lxml-xml", "html5lib"]
 
 
-def parse_svg(markup: str | bytes | SupportsRead[str] | SupportsRead[bytes]) -> Svg:
+def get_root_svg_fragments(soup: bs4.Tag) -> list[bs4.Tag]:
+    queue = deque([soup])
+
+    while queue:
+        node = queue.popleft()
+
+        svg_fragments = node.find_all("svg")
+
+        if svg_fragments:
+            return svg_fragments
+
+        queue.extend(child for child in node.children if isinstance(child, bs4.Tag))
+
+    return []
+
+
+def parse_svg(
+    markup: str | bytes | SupportsRead[str] | SupportsRead[bytes],
+    /,
+    *,
+    parser: Parser = "lxml-xml",
+) -> Svg:
     """Parse an SVG document.
 
     The document must be a valid XML document containing a single SVG document fragment.
 
     Args:
         markup: A string or a file-like object representing markup to be parsed.
+        parser: The name of the parser to use. Defaults to 'lxml-xml'.
 
     Returns:
         The parsed SVG document in the form of an `Svg` instance.
@@ -38,16 +55,16 @@ def parse_svg(markup: str | bytes | SupportsRead[str] | SupportsRead[bytes]) -> 
         1
 
     """
-    soup = BeautifulSoup(markup)
+    soup = BeautifulSoup(markup, features=parser)
 
-    svg_fragment_count = len(soup.find_all("svg", recursive=False))
+    svg_fragments = get_root_svg_fragments(soup)
 
-    if svg_fragment_count != 1:
+    if len(svg_fragments) != 1:
         msg = (
-            f"Expected 1 <svg> element, found {svg_fragment_count}."
+            f"Expected 1 <svg> element, found {len(svg_fragments)}."
             " This does not look like a valid SVG."
         )
 
         raise ValueError(msg)
 
-    return Svg(_backend=cast(bs4.Tag, soup.svg))
+    return Svg(_backend=svg_fragments[0])
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,55 @@
-def test_dummy() -> None:
-    assert True
+import bs4
+import pytest
+from bs4 import BeautifulSoup
+from hypothesis import given
+from hypothesis.strategies import integers
+
+from svglab.parse import Parser, get_root_svg_fragments
+
+
+@pytest.mark.parametrize(
+    ("markup", "fragment_count"),
+    [
+        ("a", 0),
+        ("", 0),
+        ("<svg></svg>", 1),
+        ("<svg></svg><svg></svg>", 1),
+        ("<foo><svg></svg></foo>", 1),
+    ],
+)
+def test_get_root_svg_fragments(markup: str, fragment_count: int) -> None:
+    soup = BeautifulSoup(markup, features="lxml-xml")
+
+    assert len(get_root_svg_fragments(soup)) == fragment_count
+
+
+@pytest.mark.parametrize("parser", ["lxml", "lxml-xml", "html5lib"])
+def test_get_root_svg_fragments_parser(parser: Parser) -> None:
+    soup = BeautifulSoup("<svg></svg>", features=parser)
+
+    assert len(get_root_svg_fragments(soup)) == 1
+
+
+@given(integers(min_value=0, max_value=100))
+def test_get_root_svg_fragments_simple(fragment_count: int) -> None:
+    soup = BeautifulSoup()
+
+    for _ in range(fragment_count):
+        soup.append(bs4.Tag(name="svg"))
+
+    assert len(get_root_svg_fragments(soup)) == fragment_count
+
+
+@given(integers(min_value=0, max_value=100), integers(min_value=1, max_value=100))
+def test_get_root_svg_fragments_nested(fragment_count: int, nesting_level: int) -> None:
+    elem: bs4.Tag = BeautifulSoup()
+
+    for _ in range(nesting_level):
+        tag = bs4.Tag(name="g")
+        elem.append(tag)
+        elem = tag
+
+    for _ in range(fragment_count):
+        elem.append(bs4.Tag(name="svg"))
+
+    assert len(get_root_svg_fragments(elem)) == fragment_count
