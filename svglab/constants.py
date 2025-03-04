c4c53b7b8b29c6004e938a36796042b87374e761
Tomáš Režňák <tomas.reznak@volny.cz>
2025-03-04 17:19:24 +0100

    Move Xmlns to utiltypes

--- svglab/constants.py
+++ svglab/constants.py
@@ -1,11 +1,12 @@
 import typing_extensions
-from typing_extensions import Final, Literal, TypeAlias
+from typing_extensions import Final
+
+from svglab import utiltypes
 
 
-Xmlns: TypeAlias = Literal["http://www.w3.org/2000/svg"]
-"""Type representing valid values for the `xmlns` attribute."""
-
-SVG_XMLNS: Final[Xmlns] = typing_extensions.get_args(Xmlns)[0]
+SVG_XMLNS: Final[utiltypes.Xmlns] = typing_extensions.get_args(
+    utiltypes.Xmlns
+)[0]
 """The value of the `xmlns` attribute."""
 
 FLOAT_RELATIVE_TOLERANCE: Final = 1e-9
